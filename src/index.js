861ab025e6615c6aeab9391c2adf8b174dc574f9
pauggza <pauggza@gmail.com>
2026-01-18 22:24:38 -0800

    added axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+function searchCity() {}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
